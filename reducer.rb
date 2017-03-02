2587a972a2485c59569f2c9a32d9d557d551282d
Akiya <autumnsky64@gmail.com>
2017-03-02 12:14:21 +0900

    Comment only flow outline

---
+++ reducer.rb
@@ -0,0 +1,10 @@
+# TwitterAPI認証
+
+# APIでリストAutumnのメンバーを取得
+https://api.twitter.com/1.1/lists/list.json?owner_screen_name=K_akiya?slug=autumn
+
+# APIでフォローのメンバーを取得
+
+# Autumnにないフォローメンバーをリムーブ対象メンバーとしてリストアップ
+
+# リムーブリストに従ってリムーブ実行
