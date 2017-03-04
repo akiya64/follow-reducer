1666b5052ac8a6ee1325c71da87de4cb7198980d
Akiya <autumnsky64@gmail.com>
2017-03-04 19:50:29 +0900

    Implement puts user id from autumn1 list

--- reducer.rb
+++ reducer.rb
@@ -1,7 +1,17 @@
+require 'twitter'
+
 # TwitterAPI認証
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ""
+  config.consumer_secret     = ""
+  config.access_token        = ""
+  config.access_token_secret = ""
+end
 
 # APIでリストAutumnのメンバーを取得
-https://api.twitter.com/1.1/lists/list.json?owner_screen_name=K_akiya?slug=autumn
+client.list_members("autumn1").each{ |user|
+  puts user.id
+}
 
 # APIでフォローのメンバーを取得
 
