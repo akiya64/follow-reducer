82dbf3b15ad19113c58643ced76549885317776e
Akiya <autumnsky64@gmail.com>
2017-03-04 21:40:53 +0900

    Use Yaml file

--- reducer.rb
+++ reducer.rb
@@ -1,11 +1,14 @@
 require 'twitter'
+require 'yaml'
+
+yaml = YAML.load_file("config.yaml")
 
 # TwitterAPI認証
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ""
-  config.consumer_secret     = ""
-  config.access_token        = ""
-  config.access_token_secret = ""
+  config.consumer_key        = yaml["my_consumer_key"]
+  config.consumer_secret     = yaml["my_consumer_secret"]
+  config.access_token        = yaml["my_access_token"]
+  config.access_token_secret = yaml["my_access_token_secret"]
 end
 
 # APIでリストAutumnのメンバーを取得
